e214ab246f2ebeb6a9ff456825d80ea4fb4b654d
denham <denham@denhamMacBook-Air.local>
2023-09-04 08:32:05 +0900

    fix: マークダウンライブラリをmarkdに置き換え

--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -1,26 +1,58 @@
-<script setup lang="ts">
-import { Milkdown, useEditor } from "@milkdown/vue";
-import { defaultValueCtx, Editor, rootCtx } from "@milkdown/core";
-import { nord } from "@milkdown/theme-nord";
-import { commonmark } from "@milkdown/preset-commonmark";
+<template>
+  <v-app>
+    <v-row>
+      <v-col cols="6">
+        <v-card>
+          <v-card-text>
+            <textarea
+              v-model="markdownText"
+              rows="10"
+              style="width: 100%"
+            ></textarea>
+          </v-card-text>
+        </v-card>
+      </v-col>
 
-const markdown = `# Milkdown Vue Commonmark
-z
+      <v-col cols="6">
+        <v-card>
+          <v-card-text>
+            <div v-html="parsedMarkdown"></div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-app>
+</template>
+
+<script>
+import { computed, ref } from "vue";
+import { marked } from "marked";
+
+export default {
+  setup() {
+    const markdownText = ref(`# Milkdown Vue Commonmark
+
 > You're scared of a world where you're needed.
 
-This is a demo for using Milkdown with **Vue**.`;
+This is a demo for using Milkdown with **Vue**.`);
 
-useEditor((root) => {
-  return Editor.make()
-    .config(nord)
-    .config((ctx) => {
-      ctx.set(rootCtx, root);
-      ctx.set(defaultValueCtx, markdown);
-    })
-    .use(commonmark);
-});
+    const parsedMarkdown = computed(() => {
+      return marked(markdownText.value);
+    });
+
+    return { markdownText, parsedMarkdown };
+  },
+};
 </script>
 
-<template>
-  <Milkdown />
-</template>
+<style scoped>
+.card {
+  margin: 20px;
+  padding: 20px;
+}
+.headline {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+</style>
