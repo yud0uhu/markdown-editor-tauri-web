6ceb4ed039467f58ab0564a1b54a7bfaacaec5eb
denham <denham@denhamMacBook-Air.local>
2023-09-23 09:06:57 +0900

    add: ツールバーを実装

--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -1,32 +1,57 @@
 <template>
   <v-app>
-    <v-row>
-      <v-col :style="editorStyle">
-        <v-card class="editor-card width-100">
-          <v-card-text>
-            <textarea
-              v-if="!isPreviewFullScreen"
-              v-model="markdownText"
-              :editorConfigs="editorConfigs"
-              rows="10"
-              class="editor-textarea"
-            ></textarea>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col :style="previewStyle">
-        <v-card class="preview-card width-100">
-          <v-card-text>
-            <div
-              v-if="!isEditorFullScreen"
-              rows="10"
-              v-html="parsedMarkdown"
-              class="preview-content"
-            ></div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-container fluid>
+      <v-row>
+        <v-col :style="editorStyle" class="bg-grey-darken-4">
+          <ToolBar @insert="insertMarkdownText" />
+          <v-card class="editor-card width-100 bg-grey-darken-4">
+            <v-card-text>
+              <textarea
+                @input="updateSuggestList(markdownText)"
+                autocomplete="on"
+                list="food"
+                v-if="!isPreviewFullScreen"
+                v-model="markdownText"
+                :editorConfigs="editorConfigs"
+                rows="10"
+                class="editor-textarea bg-grey-darken-4"
+              ></textarea>
+
+              <v-menu
+                v-model="showSuggest"
+                right
+                offset-y="10"
+                transition="scale-transition"
+              >
+                <v-list>
+                  <v-list-item
+                    v-for="item in suggestList"
+                    :key="item"
+                    @click="selectSuggestion(item)"
+                  >
+                    <v-list-item-content>
+                      <v-list-item-title>{{ item }}</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col :style="previewStyle">
+          <v-card class="preview-card width-100">
+            <v-card-text>
+              <div
+                v-if="!isEditorFullScreen"
+                rows="10"
+                v-html="parsedMarkdown"
+                class="preview-content"
+              ></div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -35,6 +60,7 @@ import hljs from "highlight.js";
 import { reactive, computed, ref, defineProps, watch } from "vue";
 import MarkdownIt from "markdown-it";
 import "highlight.js/styles/nord.css";
+import ToolBar from "./ToolBar.vue";
 
 const md = new MarkdownIt();
 const emits = defineEmits(["update:onMarkdownUpdate", "update:markdownText"]);
@@ -50,6 +76,11 @@ const isEditorFullScreen = ref(false);
 const isPreviewFullScreen = ref(false);
 const markdownText = ref("");
 
+const insertMarkdownText = (content: string) => {
+  markdownText.value += content;
+  console.log("Markdownが更新されました:", content);
+  emits("update:markdownText", content);
+};
 const onMarkdownUpdate = (value: string) => {
   emits("update:onMarkdownUpdate", value);
   emits("update:markdownText", value);
@@ -73,9 +104,9 @@ const parsedMarkdown = computed(() => {
 md.options.highlight = function (code, lang) {
   lang = lang || "plaintext";
   const validLanguage = hljs.getLanguage(lang) ? lang : "plaintext";
-      return `<pre><code class="hljs ${validLanguage}">${
-        hljs.highlight(validLanguage, code).value
-      }</code></pre>`;
+  return `<pre><code class="hljs ${validLanguage}">${
+    hljs.highlight(validLanguage, code).value
+  }</code></pre>`;
 };
 
 const editorConfigs = reactive({
@@ -95,6 +126,56 @@ const editorStyle = computed(() => ({
 const previewStyle = computed(() => ({
   display: props.isEditorFullScreen ? "none" : "block",
 }));
+const inputText = ref("");
+const suggestList = ref([""]);
+const showSuggest = ref(false);
+const updateSuggestList = (markdownText: string) => {
+  const input = inputText.value.toLowerCase();
+  console.log(markdownText);
+  // TODO:hサジェスト
+  if (markdownText.trim().startsWith("#")) {
+    suggestList.value = [
+      "# h1",
+      "## h2",
+      "### h3",
+      "#### h4",
+      "##### h5",
+      "###### h6",
+    ].filter((item) => item.toLowerCase().includes(input));
+  }
+
+  // if (/^:/.test(markdownText)) {
+  //   suggestList.value = [
+  //     ":smile:",
+  //     ":heart:",
+  //     ":star:",
+  //     ":+1:",
+  //     ":-1:",
+  //     ":bell:",
+  //     ":fire:",
+  //     ":ok_hand:",
+  //     ":rocket:",
+  //     ":speech_balloon:",
+  //     ":mag:",
+  //     ":bulb:",
+  //     ":moneybag:",
+  //     ":camera:",
+  //     ":trophy:",
+  //     ":gift:",
+  //     ":hourglass:",
+  //     ":envelope:",
+  //     ":link:",
+  //     ":lock:",
+  //   ].filter((item) => item.toLowerCase().includes(input));
+  //   showSuggest.value = !!suggestList.value.length;
+  // }
+};
+const selectSuggestion = (suggestion: string) => {
+  const newText = markdownText.value + suggestion.substring(1);
+  markdownText.value = newText;
+  inputText.value = "";
+  showSuggest.value = false;
+};
 </script>
 
 <style scoped>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-toolbar class="bg-black">
+    <v-btn @click="InsertBold()">
+      <b>B</b>
+    </v-btn>
+    <v-btn @click="InsertItalic()">
+      <i>I</i>
+    </v-btn>
+    <v-btn @click="InsertStrikethrough()">
+      <s>abc</s>
+    </v-btn>
+    <v-btn @click="InsertHeading()">
+      <h>H</h>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn @click="InsertCheckList()">
+      <v-icon>mdi-check-box-outline</v-icon>
+    </v-btn>
+    <v-btn @click="InsertLink()">
+      <v-icon>mdi-link</v-icon>
+    </v-btn>
+    <v-btn @click="InsertImage()">
+      <v-icon>mdi-image</v-icon>
+    </v-btn>
+    <v-btn @click="InsertTable()">
+      <v-icon>mdi-table</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script setup lang="ts">
+import { ref, defineEmits } from "vue";
+
+const emits = defineEmits(["insert"]);
+
+const InsertTable = () => {
+  emitInsert(
+    "\n" +
+      "| Column 1 | Column 2 | Column 3 |\n" +
+      "| -------- | -------- | -------- |\n" +
+      "| Text     | Text     | Text     |\n"
+  );
+};
+const InsertImage = () => {
+  emitInsert("![alt](https://)");
+};
+const InsertLink = () => {
+  emitInsert("[](https://)");
+};
+const InsertCheckList = () => {
+  emitInsert("- [ ] ");
+};
+const InsertBold = () => {
+  emitInsert("****");
+};
+const InsertItalic = () => {
+  emitInsert("**");
+};
+const InsertStrikethrough = () => {
+  emitInsert("~~~~");
+};
+const InsertHeading = () => {
+  emitInsert("#");
+};
+
+const emitInsert = (content: string) => {
+  if (typeof content === "string") {
+    console.log(content);
+    emits("insert", content);
+  }
+};
+</script>
+<style scoped>
+.toolbar {
+  flex-wrap: nowrap;
+}
+.toolbar button {
+  margin: 0 4px;
+  min-width: 36px;
+  min-height: 36px;
+  padding: 0;
+}
+.toolbar button i,
+.toolbar button b,
+.toolbar button s {
+  font-size: 1.5em;
+}
+</style>
